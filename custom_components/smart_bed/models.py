8b4355777940470520fe756f8e4535ece093a950
William Palm <wipal001@gmail.com>
2024-02-27 00:17:08 +0100

    Fix import statement for SmartBedDevice

--- custom_components/smart_bed/models.py
+++ custom_components/smart_bed/models.py
@@ -1,7 +1,7 @@
 """The Smart Bed integration models."""
 from __future__ import annotations
 from dataclasses import dataclass
-from smart_bed_device import SmartBedDevice
+from .smart_bed_device import SmartBedDevice
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 
