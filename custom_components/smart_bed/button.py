352a8cdb5aff3eb13b9b511ff1487dd5aba29726
William Palm <wipal001@gmail.com>
2024-02-27 23:52:12 +0100

    Refactor button integration and add support for different button types

--- custom_components/smart_bed/button.py
+++ custom_components/smart_bed/button.py
@@ -1,22 +1,22 @@
 """Platform for button integration."""
 from __future__ import annotations
 import logging
-from .smart_bed_device import SmartBedDevice
-from .models import SmartBedData
 from homeassistant.components.button import ButtonEntity
 from .const import DOMAIN
+from .smart_bed_device import SmartBedDevice
+from .coordinator import SmartBedCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-# TODO: Add all supported commands
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add sensors for passed config_entry in HA."""
-    data: SmartBedData = hass.data[DOMAIN][config_entry.entry_id]
+    coordinator: SmartBedCoordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     async_add_entities([
-        StartWaveButton(data.device)
+        StartWaveButton(coordinator.api)
     ])
+
 
 class ButtonBase(ButtonEntity):
     def __init__(self, device: SmartBedDevice):
@@ -27,6 +27,72 @@ class ButtonBase(ButtonEntity):
         """Return information to link this entity with the correct device."""
         return {"identifiers": {(DOMAIN, self._device.identifier)}}
 
+
+class DownButton(ButtonBase):
+    def __init__(self, device: SmartBedDevice):
+        super().__init__(device)
+        self._attr_unique_id = f"{self._device.identifier}_down_button"
+        self._attr_name = f"{self._device.name} Down Button"
+
+    async def async_press(self) -> None:
+        _LOGGER.debug("Pressed button")
+        await self._device.down()
+
+
+class UpButton(ButtonBase):
+    def __init__(self, device: SmartBedDevice):
+        super().__init__(device)
+        self._attr_unique_id = f"{self._device.identifier}_up_button"
+        self._attr_name = f"{self._device.name} Up Button"
+
+    async def async_press(self) -> None:
+        _LOGGER.debug("Pressed button")
+        await self._device.up()
+
+
+class HeadDownButton(ButtonBase):
+    def __init__(self, device: SmartBedDevice):
+        super().__init__(device)
+        self._attr_unique_id = f"{self._device.identifier}_head_down_button"
+        self._attr_name = f"{self._device.name} Head Down Button"
+
+    async def async_press(self) -> None:
+        _LOGGER.debug("Pressed button")
+        await self._device.head_down()
+
+
+class HeadUpButton(ButtonBase):
+    def __init__(self, device: SmartBedDevice):
+        super().__init__(device)
+        self._attr_unique_id = f"{self._device.identifier}_head_up_button"
+        self._attr_name = f"{self._device.name} Head Up Button"
+
+    async def async_press(self) -> None:
+        _LOGGER.debug("Pressed button")
+        await self._device.head_up()
+
+
+class LegsDownButton(ButtonBase):
+    def __init__(self, device: SmartBedDevice):
+        super().__init__(device)
+        self._attr_unique_id = f"{self._device.identifier}_legs_down_button"
+        self._attr_name = f"{self._device.name} Legs Down Button"
+
+    async def async_press(self) -> None:
+        _LOGGER.debug("Pressed button")
+        await self._device.legs_down()
+
+
+class LegsUpButton(ButtonBase):
+    def __init__(self, device: SmartBedDevice):
+        super().__init__(device)
+        self._attr_unique_id = f"{self._device.identifier}_legs_up_button"
+        self._attr_name = f"{self._device.name} Legs Up Button"
+
+    async def async_press(self) -> None:
+        _LOGGER.debug("Pressed button")
+        await self._device.legs_up()
+        
 class StartWaveButton(ButtonBase):
     def __init__(self, device: SmartBedDevice):
         super().__init__(device)
